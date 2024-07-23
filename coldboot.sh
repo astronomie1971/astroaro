60694fffac38dea96d25107882b2a581bc87e3d7
astronomie1971 <143555571+astronomie1971@users.noreply.github.com>
2024-07-23 21:52:20 +0200

    commit before install

---
+++ coldboot.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd /home/astroberry/Downloads/observatory
+
+cp test.txt boot.txt
+cp test.txt test2.txt
---
+++ dht22.h
@@ -0,0 +1,9 @@
+/*  H files */
+
+
+
+#define DHT_PIN_INT		2	/* GPIO-2 pin 11 */
+
+
+// function prototypes
+void read_dht_data(int DHT_PIN,float *temp_degre,float *temp_fahren,float *humidity);
